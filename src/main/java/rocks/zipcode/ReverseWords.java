fca480429d386c5b9d7411f694c7de677b706602
olamide <oadams@fandm.edu>
2023-11-22 01:35:28 -0500

    made some changes to my code

--- src/main/java/rocks/zipcode/ReverseWords.java
+++ src/main/java/rocks/zipcode/ReverseWords.java
@@ -1,20 +1,29 @@
 package rocks.zipcode;
-
+import java.util.regex.Pattern;
 public class ReverseWords {
     public static String reverseWords(String s) {
 
-       // this works when you want to reverse letters or characters in a string.
+        // this works when you want to reverse letters or characters in a word
+        //i need to reverse the string by word and ensure that the characters remain in the same order in each word
+        //of the string
 //        char c;
 //        String d = "";
 //        for (int i = 0; i <s.length(); i++) {
 //            c = s.charAt(i);
 //            d = c + d;
 
+        Pattern p = Pattern.compile("\s"); // the string is split when there are whitespace characters
+        String[] temp = p.split(s); // and stored in a String array named temp (short for temporary)
+        String reverse = ""; // an empty string is created to be able to put in the new string in reverse
+        for (int i = 0; i < temp.length; i++) { // for loop is used to store that string in reverse by iterating over
+            // the temporary string
+            if (i== temp.length -1)
+                reverse = temp[i] + reverse;
+            else
+                reverse = " " + temp[i] + reverse;
         }
-
-        return ;
-
-
+        return reverse; // return the new string with the words in reverse
+    }
 
     }
-}
+
